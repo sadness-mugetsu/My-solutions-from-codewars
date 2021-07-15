275f1611da1b5642472dbbf508da9a1f795973f5
sadness-mugetsu <ichigo.kurosaki.02@inbox.ru>
2021-07-15 18:22:57 +0400

    Added new solution

--- My solutions from codewars/UnitTest1.cs
+++ My solutions from codewars/UnitTest1.cs
@@ -3,12 +3,14 @@ using System;
 
 namespace My_solutions_from_codewars
 {
+    
+    /*Sulution 15.07.21
+    /link on task https://www.codewars.com/kata/55a70521798b14d4750000a4/train/csharp
+    
     public static class Kata
     {
-        public static string Greet(string name)
-        {
-            throw new NotImplementedException("Greet is not implemented.");
-        }
+        public static string Greet(string name) => $"Hello, {name} how are you doing today?";
+
     }
 
     [TestFixture]
@@ -19,5 +21,5 @@ namespace My_solutions_from_codewars
         {
             Assert.That(Kata.Greet("Ryan"), Is.EqualTo("Hello, Ryan how are you doing today?"));
         }
-    }
+    }*/
 }
